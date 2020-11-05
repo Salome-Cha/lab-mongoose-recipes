ea55cd81387fdae5711b7b26a2bb9d319e407680
Salome Charpignon <salome.charpignon@gmail.com>
2020-11-05 12:17:50 +0000

    Initial commit of mongoose lab

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,9 +20,46 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  // CREATE A NEW RECIPE BY CHAINING THE CREATE function:
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(
+      {
+            title: 'Caesar Salad',
+            level:'Easy Peasy',
+            ingredients: ['salad', 'tomatoes', 'chicken', 'toasts', 'sauce'],
+            cuisine: 'French',
+            dishType: 'main_course',
+            image: 'https://images.media-allrecipes.com/images/75131.jpg',
+            duration: 20,
+            creator: 'Chef Mélissa',
+            created:  2020-11-05
+      }
+    )
+  })  
+  .then((recipe) => {
+    console.log (`A new ${recipe.title} recipe was added`);
+  })
+  .then (() => {
+    return Recipe.insertMany(data)})
+  .then(() => { 
+    console.log("The array of data coming from JSON file was inserted in the database")  // Insertion Success
+  })
+  .then(()=>{
+     return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, { $set:  {duration: 100}})  
+  })
+  .then (() => {
+    console.log('The recipe was update')   // update Success
+  })
+  .then(() => {
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+  .then(() =>{
+  console.log('The carrot cake was deleted')    // deletion Success
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+     console.error('Error during the process', error);
+  })
+ .finally(()=> {
+    mongoose.connection.close()    // connection closing.
+  })
+
